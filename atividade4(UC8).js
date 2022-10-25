48ffbf97860fa97b8dbbce69b4d168159b2d9d6b
sovietkilla07 <sovietmoderator109@gmail.com>
2022-10-25 00:00:13 -0300

    Create atividade4(UC8).js
    atividade 4 da UC8

---
+++ atividade4(UC8).js
@@ -0,0 +1,13 @@
+let numeroDeParticipantes = 97
+if (numeroDeParticipantes >= 100){
+    console.log("Lamento, mas o número de participantes já chegou ao limite.")
+}else{
+    console.log("Há espaço para participantes.")
+}
+
+let idade = 18
+if (idade >= 18){
+    console.log("Cadastro concluído com sucesso! :)")
+}else{
+    console.log("Todo o participante deve ter, no mínimo, 18 anos. Você não tem a idade mínima.")
+}
